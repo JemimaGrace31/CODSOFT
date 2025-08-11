92f2242cc33d08f100424ce05904ccdb5108aecd
JemimaGrace31 <jemimagrace3108@gmail.com>
2025-08-11 12:05:54 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+function appendChar(char) {
+    document.getElementById("display").value += char;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteChar() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    let display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
